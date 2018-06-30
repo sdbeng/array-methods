ed5b42fd73b821f4ecc5e83e5b41a5dfd39aff4b
Sergio Barrera <sdbeng@gmail.com>
2018-06-29 23:43:49 -0700

    more concepts and flattening arrays of arrays

--- reduce2.js
+++ reduce2.js
@@ -33,4 +33,19 @@ const above30 = euro.reduce((total, amount) => {
 }, []);
 above30 // [ 41.85, 46.5 ]
 
-// 
+/*  The benefit of using reduce comes into play when you want to map and filter together and you have a lot of data to go over.
+
+If you chain map and filter together you are doing the work twice. You filter every single value and then you map the remaining values. With reduce you can filter and then map in a single pass.
+
+Use map and filter but when you start chaining lots of methods together you now know that it is faster to reduce the data instead.
+*/
+
+// Flattening an array of arrays
+// We can use reduce to flatten nested amounts into a single array.
+
+// We set the initial value to an empty array and then concatenate the current value to the total.
+const data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const flat = data.reduce((total, amount) => {
+  return total.concat(amount);
+}, []);
+flat // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
