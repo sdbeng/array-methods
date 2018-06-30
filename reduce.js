e046b501dd7294cbec70b8a4101f9f505ac77d3a
Sergio <sdbeng@gmail.com>
2018-06-29 22:08:21 -0700

    Create reduce.js

---
+++ reduce.js
@@ -0,0 +1,12 @@
+let votes = ["ruby","react","angular","python","react","vue","vue","ruby","python","vanilla js","vanilla js"]
+
+let initialValue = {}
+
+const reducer = votes.reduce((accum, currValue) => {
+  if(!accum[currValue)){
+    accum[currValue] = 1
+  }else {
+    accum[currValue] += 1
+  }
+  return accum
+}, 0)
